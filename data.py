2dd15fc971b8e7d8ae4ec25408f8a81e5e807ea1
David Montoya <masda70@gmail.com>
2015-11-01 22:32:32 +0100

    code clean up

---
+++ data.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+
+def posts():
+    print "Reading posts..."
+    data = pd.read_csv("data/posts.csv", dtype={'cooked': np.str}, na_values=[],
+                       keep_default_na=False, encoding="utf-8")
+    return data
